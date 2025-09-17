435ee2a154595068e58d6214cfcf4ad9d7ad8e0b
edmoore05 <ethandmoore05@gmail.com>
2025-09-17 12:19:10 -0400

    Several improvements to efficiency

--- thresholding.py
+++ thresholding.py
@@ -12,21 +12,18 @@ import os
 excel_file_location = "C:\\Users\\Ethan\\OneDrive - Westminster College\\Lignin_Biofilm_Project\\python_image_analysis.xlsx"
 
 #Filtering so that pixels that are outside the range of mean +/- 10*std are set to 0
-low_cutoff = float()
-hight_cutoff = float()
-def filter_image(img, low_cutoff=5, high_cutoff=84):
+def filter_image(img):
     mean = mean_intensity(img)
     std = np.std(img)
     low_cutoff = mean - std*10
-    hight_cutoff = mean + std*10
-    gray_image_filtered = np.copy(gray_image)
-    gray_image_filtered[gray_image < low_cutoff] = 0
-    gray_image_filtered[gray_image > hight_cutoff] = 0
+    high_cutoff = mean + std*10
+    gray_image_filtered = np.copy(img)
+    gray_image_filtered[(img < low_cutoff) | (img > high_cutoff)] = 0
     return gray_image_filtered
 
 #Blurs image as a another way to decrease noise
 def blur_image(img, kernel_size=(5,5), sigma=0):
-    blur = cv2.GaussianBlur(img, (5,5), 0)
+    blur = cv2.GaussianBlur(img, kernel_size, sigma)
     return blur
 
 #Figure out %Area of white pixels
@@ -47,28 +44,6 @@ def mean_intensity(img):
     mean_val = np.mean(img)
     return mean_val
 
-#Append results to excel sheet
-def append_results(path, row_dict, sheet_name="Sheet1"):
-    new_df = pd.DataFrame([row_dict])
-
-    try:
-        # Load existing workbook
-        book = load_workbook(path)
-
-        # Find the last row in the existing sheet
-        startrow = book[sheet_name].max_row
-
-        # Append new row without header
-        with pd.ExcelWriter(path, engine="openpyxl", mode="a", if_sheet_exists="overlay") as writer:
-            new_df.to_excel(writer, sheet_name=sheet_name,
-                            startrow=startrow, index=False, header=False)
-        file_name = os.path.basename(file)
-        print(f"Appended {file_name} to {path}")
-
-    except PermissionError:
-        print(f"PermissionError: {path} is locked (Excel or OneDrive may have it open). Close it and try again.")
-
-
 '''
 Start of program execution
 '''
@@ -80,12 +55,16 @@ folder_path = filedialog.askdirectory(title='Select an image folder')
 if not folder_path:
     print("No file selected. Exiting.")
     exit()
+
+#list used for holding results until for loop is finished
+results = []
+
 for file in os.listdir(folder_path):
     #Check if file is a .tif, .tiff
     ext = os.path.splitext(file)[1].lower()  # get file extension
     valid_exts = ['.tif', '.tiff']
     if ext not in valid_exts:
-        print(f"Skipping (not an image): {file}")
+        print(f'Skipping "{file}"(not an image)')
         continue
 
     file_path = os.path.join(folder_path, file)
@@ -93,7 +72,7 @@ for file in os.listdir(folder_path):
 
     #Sets image colors to B&W
     gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray_image_filtered = filter_image(gray_image, low_cutoff, hight_cutoff)
+    gray_image_filtered = filter_image(gray_image)
 
     blur = blur_image(gray_image_filtered, (5,5), 0)
 
@@ -115,12 +94,26 @@ for file in os.listdir(folder_path):
     #save_path = 'C:/Users/Ethan/Downloads/gray_image_filtered_1.png'
     #cv2.imwrite(save_path, gray_image_filtered)
 
-    print(f'Image: {os.path.basename(file)}\nPercent coverage: {percentage:.2f}% \nMedian: {median_val} \nMean: {mean_val}\nStd: {np.std(gray_image_filtered)}')
-    # #Check you have an excel file location then adds the data
-    # if excel_file_location is not None:
-    #     new_row = {'Image_Name': os.path.basename(file_path), 
-    #                'White_Area_Percentage': percentage, 
-    #                'Median_Intensity': median_val, 
-    #                'Mean_Intensity': mean_val
-    #     }
-    #     append_results(excel_file_location, new_row, sheet_name='Sheet1')
+    #Check you have an excel file location then adds the data
+    if excel_file_location is not None:
+        results.append({
+          "Image_Name": file,
+          "White_Area_Percentage": round(percentage, 2),
+          "Median_Intensity": round(median_val, 2),
+          "Mean_Intensity": round(mean_val, 2),
+          "Std_Dev": round(np.std(gray_image_filtered), 2)
+        })
+    else:
+        print("No excel file location specified. Skipping saving results.")
+        break
+    
+    #Prints results to console
+    print(f'Image: {os.path.basename(file)}\nPercent coverage: {percentage:.2f}% \nMedian: {median_val:.2f} \nMean: {mean_val:.2f}\nStd: {np.std(gray_image_filtered):.2f}\n-------------------------')
+
+#Saves results to excel file
+if results:
+    df = pd.DataFrame(results)
+#     df.to_excel(excel_file_location, sheet_name="Sheet1", index=False)
+#     print(f"Saved {len(results)} results to {excel_file_location}")
+else:
+    print("No results to save.")
